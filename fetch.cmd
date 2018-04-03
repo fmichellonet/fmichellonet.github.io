d28eeae3f6d08e99d579f67b5290fbb7465435dc
Fabrice Michellonet <fabrice.michellonet@gmail.com>
2018-04-03 21:17:48 +0200

    J'recommence, je git mon blog!

--- fetch.cmd
+++ fetch.cmd
@@ -1,5 +1,5 @@
 wget --mirror --adjust-extension --page-requisites --execute robots=off --convert-links --user-agent=Mozilla http://localhost:2368 --directory-prefix="c:\fmichellonet.github.io"
-rmdir /S/Q c:\fmichellonet.github.io\localhost
+REM rmdir /S/Q c:\fmichellonet.github.io\localhost
 ren "c:\fmichellonet.github.io\localhost+2368" localhost
 xcopy "c:\fmichellonet.github.io\localhost\*" "c:\fmichellonet.github.io\" /E /H /R /X /Y /I /K
 rmdir /S/Q c:\fmichellonet.github.io\localhost
